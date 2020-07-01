00d8b6ee5ec885d4dac53e454ded4cecde56aaf8
tymzap <tymzap@gmail.com>
2020-07-01 21:23:42 +0200

    Add forcing sequelize sync when in development environment

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,8 +5,10 @@ import sequelize from './services/sequelize';
 
 const port = process.env.PORT;
 
-sequelize.sync().then(async () => {
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+sequelize
+  .sync({ force: process.env.NODE_ENV === 'development' })
+  .then(async () => {
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
   });
-});
