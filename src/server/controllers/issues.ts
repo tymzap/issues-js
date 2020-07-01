fd8924c341d6f3e7be931fa5ace9583272d53716
tymzap <tymzap@gmail.com>
2020-07-01 23:57:33 +0200

    Add todo comments

--- src/server/controllers/issues.ts
+++ src/server/controllers/issues.ts
@@ -3,12 +3,14 @@ import Issue from '../database/models/issue';
 export const createIssue = async (
   data: Pick<Issue, 'name' | 'description' | 'userId'>
 ) =>
+  // todo add validation schema for create endpoints
   Issue.create({
     ...data,
     status: 'OPEN'
   });
 
 export const readIssues = async (userId: number) =>
+  // todo protect routes by JWT
   Issue.findAll({
     where: {
       userId
@@ -19,6 +21,7 @@ export const updateIssue = async (
   issueId: number,
   data: Partial<Pick<Issue, 'name' | 'description' | 'status'>>
 ) =>
+  // todo add validation schema for update endpoints
   !!Issue.update(data, {
     where: {
       id: issueId
